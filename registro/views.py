9134c08fae671b45a65be8f2531db1e3efbbb710
Michel-Rooney <michelrooney16@gmail.com>
2022-08-01 20:55:55 -0300

    Adicionando o gerenciamento de espaços e a remoção deles

--- registro/views.py
+++ registro/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import get_object_or_404, render, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render, HttpResponse
+import os
+
+from ninnahub.settings import BASE_DIR
 from .models import Dados, Espacos
 
 def home(request):
-    espaco = Espacos.objects.all()
+    espaco = Espacos.objects.order_by('nome').all()
 
     espacos_a_exibir = {
         'espacos' : espaco
@@ -31,4 +34,34 @@ def registro(request):
     return render(request, 'registro.html')
 
 
+
+
+
+def gerenciar_espaco(request):
+    return render(request, 'espacos/gerenciar_espaco.html')
+
+def adicionar_espaco(request):
+    return render(request, 'espacos/adicionar_espaco.html')
+
+def remover_espaco(request, espaco_id=None):
+    if request.method == 'POST':
+        try:
+            deletar = request.POST['deletar']
+        except:
+            return redirect('/remover_espaco')
         
+        if deletar:
+            espaco = Espacos.objects.order_by('nome').get(id=espaco_id)
+            os.remove(os.path.join(BASE_DIR, espaco.imagem1.path))
+            espaco.delete()
+            return redirect('/remover_espaco')
+
+
+    espaco = Espacos.objects.all()
+    espacos_a_exibir = {
+        'espacos' : espaco
+    }
+
+    return render(request, 'espacos/remover_espaco.html', espacos_a_exibir)
+
+
